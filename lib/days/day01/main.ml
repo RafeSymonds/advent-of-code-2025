c4accc19438c51aee7be8e18744548090997f957
Rafe <rsymonds@umich.edu>
2025-12-01 19:52:27 -0500

    day 1 done

--- lib/days/day01/main.ml
+++ lib/days/day01/main.ml
@@ -4,6 +4,27 @@ let read_input () =
   in
   loop []
 
+(* part 1 *)
+(* let run () = *)
+(*   let _, cnt = *)
+(*     read_input () *)
+(*     |> List.map (fun s -> *)
+(*         (String.sub s 0 1, String.sub s 1 (String.length s - 1) |> int_of_string)) *)
+(*     |> List.fold_left *)
+(*          (fun (current, cnt) (dir, value) -> *)
+(*            let current = current + if dir = "L" then -value else value in *)
+(*            let current = current mod 100 in *)
+(**)
+(*            let cnt = if current = 0 then cnt + 1 else cnt in *)
+(**)
+(*            (current, cnt)) *)
+(*          (50, 0) *)
+(*   in *)
+(*   Printf.printf "%d\n" cnt; *)
+(*   () *)
+(**)
+
+(* part 2 *)
 let run () =
   let _, cnt =
     read_input ()
@@ -11,11 +32,18 @@ let run () =
         (String.sub s 0 1, String.sub s 1 (String.length s - 1) |> int_of_string))
     |> List.fold_left
          (fun (current, cnt) (dir, value) ->
-           let current = current + if dir = "L" then -value else value in
+           let rec calc current cnt value =
+             if value = 0 then (current, cnt)
+             else
+               let current = current + if dir = "L" then -1 else 1 in
+               let current = current mod 100 in
 
-           let cnt = if current = 0 then cnt + 1 else cnt in
+               let cnt = if current = 0 then cnt + 1 else cnt in
 
-           (current, cnt))
+               calc current cnt (value - 1)
+           in
+
+           calc current cnt value)
          (50, 0)
   in
   Printf.printf "%d\n" cnt;
