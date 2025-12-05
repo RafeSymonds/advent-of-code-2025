ca129b2b680a60cdab97ad800c5061841bce1b2b
Rafe <rsymonds@umich.edu>
2025-12-05 14:13:51 -0500

    day 5 setup

---
+++ bin/day05/day05.ml
@@ -0,0 +1,1 @@
+let () = Aoc_lib.Days.Day05.Main.run ()
---
+++ lib/days/day05/main.ml
@@ -0,0 +1,9 @@
+let read_input () =
+  let rec loop acc =
+    try loop (read_line () :: acc) with End_of_file -> List.rev acc
+  in
+  loop []
+
+let run () =
+  let _input = read_input () in
+  Printf.printf "TODO: implement Day05\n"
